4b39ce78dec36890e1bb1a2a14b1787038f2d209
spirtix <michal.prazak1@gmail.com>
2018-09-03 10:10:31 +0200

    ADD cpu governor "blu_active"

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -412,6 +412,10 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
 #endif
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_ACTIVE)
+extern struct cpufreq_governor cpufreq_gov_blu_active;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_blu_active)
+#endif
 
 
 /*********************************************************************
