1647bf399a692553144a3088e6ada0e3ce4fcea2
Spirtix <Michal.prazak1@gmail.com>
2018-09-03 14:43:31 +0200

    Fix CONFIG_CPU_FREQ_DEFAULT_GOV_OPTDEMAND

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -414,6 +414,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_ACTIVE)
 extern struct cpufreq_governor cpufreq_gov_blu_active;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_blu_active)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_OPTDEMAND)
+extern struct cpufreq_governor cpufreq_gov_optdemand;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_optdemand)
 #endif
 
 
